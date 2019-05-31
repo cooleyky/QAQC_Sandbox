b19bbd58a31184b9df9a44ac534aa3c9d8e5e841
Andrew <andrew.c.reed88@gmail.com>
2019-05-31 15:18:32 -0400

    Another Metadata Review directory reorg update.

--- Metadata_Review/utils.py
+++ Metadata_Review/utils.py
@@ -5,6 +5,7 @@ import re
 import numpy as np
 import pandas as pd
 import shutil
+import PyPDF2
 from wcmatch import fnmatch
 
 
@@ -233,6 +234,33 @@ def splitDataFrameList(df, target_column):
     df.apply(splitListToRows, axis=1, args=(new_rows, target_column))
     new_df = pd.DataFrame(new_rows)
     return new_df
+
+
+def split_multipage_pdfs(directory, pdfname):
+    """
+    This function splits a multipage pdf into its
+    constituent pages.
+
+    Args:
+            directory - the full path to the directory
+                    where the pdf file to be split is saved.
+            pdfname - the name of the pdf file to be split
+    Returns:
+            pdfname_page*_.pdf - each of the individual
+                    pages of the pdf written to the same
+                    directory as the original pdf.
+
+    """
+
+    filepath = '/'.join((directory, pdfname))
+    inputpdf = PyPDF2.PdfFileReader(filepath, 'rb')
+
+    for i in range(inputpdf.numPages):
+        output = PyPDF2.PdfFileWriter()
+        output.addPage(inputpdf.getPage(i))
+        filename = '_'.join((filepath.split('.')[0], 'page', str(i)))
+        with open(filename+'.pdf', 'wb') as outputStream:
+            output.write(outputStream)
 
 
 def generate_file_path(dirpath, filename, ext=['.cap', '.txt', '.log'], exclude=['_V', '_Data_Workshop']):
